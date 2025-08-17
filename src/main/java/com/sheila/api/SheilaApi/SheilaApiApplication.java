20792f90352120aa52553bbcce62deb99e109fef
Sefa <sefakesici1220@gmail.com>
2025-08-17 04:15:15 +0300

    Normalize line endings (LF in repo; CRLF for .cmd/.bat)

---
+++ src/main/java/com/sheila/api/SheilaApi/SheilaApiApplication.java
@@ -0,0 +1,13 @@
+package com.sheila.api.SheilaApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SheilaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SheilaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sheila/api/SheilaApi/SheilaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sheila.api.SheilaApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SheilaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
